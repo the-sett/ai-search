8a0f9af24322941c2dd91c8ffadecd234710d7c0
Rupert Smith <github@badgerhouse.freeserve.co.uk>
2017-05-16 14:20:24 +0100

    Initial import.

---
+++ example/RiverCrossing.elm
@@ -0,0 +1,134 @@
+module RiverCrossing exposing (uninformed, start)
+
+import EveryDict as Dict exposing (EveryDict)
+import Search
+import Maybe.Extra
+
+
+type Character
+    = Farmer
+    | Wolf
+    | Goat
+    | Cabbage
+
+
+type Position
+    = West
+    | East
+
+
+type alias State =
+    EveryDict Character Position
+
+
+characters : List Character
+characters =
+    [ Farmer, Wolf, Goat, Cabbage ]
+
+
+start : State
+start =
+    Dict.empty
+        |> Dict.insert Farmer West
+        |> Dict.insert Wolf West
+        |> Dict.insert Goat West
+        |> Dict.insert Cabbage West
+
+
+switch position =
+    case position of
+        East ->
+            West
+
+        West ->
+            East
+
+
+
+{- Moves the specified character to the opposite bank.
+   * The farmer must always be in the boat for the move, so moving a non-farmer
+     will automatically include the farmer in the move.
+   * When moving a non-farmer the farmer must be on the same side as the thing
+     being moved, or else the boat will not be there to make the move.
+   * States resulting in the goat or cabbage being eaten will result in Nothing.
+   * The goal state where all are on the West bank will be marked as succesfull.
+-}
+
+
+move : Character -> Search.Node State -> Maybe (Search.Node State)
+move character ( state, _ ) =
+    let
+        nextState =
+            if character == Farmer then
+                Dict.update Farmer (Maybe.map switch) state |> Just
+            else
+                let
+                    farmerPos =
+                        Dict.get Farmer state
+
+                    characterPos =
+                        Dict.get character state
+                in
+                    if (farmerPos == characterPos) then
+                        Just <|
+                            Dict.update Farmer (Maybe.map switch) <|
+                                Dict.update character (Maybe.map switch) state
+                    else
+                        Nothing
+    in
+        Maybe.Extra.filter (not << illegal) nextState
+            |> Maybe.andThen (\state -> Just ( state, goal state ))
+
+
+
+{- Checks if a state results in the goat or cabbage being eaten. -}
+
+
+illegal : State -> Bool
+illegal state =
+    let
+        farmerFlip =
+            (Maybe.map switch) <| Dict.get Farmer state
+
+        wolf =
+            Dict.get Wolf state
+
+        goat =
+            Dict.get Goat state
+
+        cabbage =
+            Dict.get Cabbage state
+    in
+        (farmerFlip == wolf && farmerFlip == goat)
+            || (farmerFlip == goat && farmerFlip == cabbage)
+
+
+
+{- Checks if a state matches the goal of everthing safely on the East bank. -}
+
+
+goal : State -> Bool
+goal state =
+    List.foldl (\character result -> ((Dict.get character state) == Just East) && result) True characters
+
+
+
+{- Produces new states from a given state, by attempting to move each of the
+   characters in turn to see if that produces a valid new state.
+-}
+
+
+step : Search.Step State
+step node =
+    List.filterMap (\character -> move character node)
+        characters
+
+
+
+{- Packages the search as an uninformed search. -}
+
+
+uninformed : Search.Uninformed State
+uninformed =
+    { step = step
+    }
---
+++ src/Search.elm
@@ -0,0 +1,117 @@
+module Search
+    exposing
+        ( Node
+        , SearchResult(..)
+        , Step
+        , Uninformed
+        , search
+        , next
+        , nextGoal
+        )
+
+{- Defines the type of Nodes that searches work over. -}
+
+
+type alias Node state =
+    ( state, Bool )
+
+
+
+{- Defines the possible outcomes of a search. -}
+
+
+type SearchResult state
+    = Complete
+    | Goal state
+    | Ongoing state (() -> SearchResult state)
+
+
+
+{- Defines the type of the step function that produces new states from existing
+   ones. This is how the graph over the search space is defined.
+-}
+
+
+type alias Step state =
+    Node state -> List (Node state)
+
+
+
+{- Defines the type of a bundle of operators that need to be supplied to conduct
+   an uninformed (non-heuristic) search.
+-}
+
+
+type alias Uninformed state =
+    { step : Step state
+    }
+
+
+
+{- Performs an uninformed search. -}
+
+
+search : Uninformed state -> List (Node state) -> SearchResult state
+search uninformed start =
+    let
+        step =
+            uninformed.step
+
+        expandHead : List (Node state) -> SearchResult state
+        expandHead pending =
+            case pending of
+                [] ->
+                    Complete
+
+                ( state, True ) :: _ ->
+                    Goal state
+
+                ( state, False ) :: xs ->
+                    Ongoing (Debug.log "search" state)
+                        (\() ->
+                            expandHead <|
+                                List.foldl (\node buffer -> buffer ++ [ node ]) xs (step ( state, False ))
+                        )
+    in
+        expandHead start
+
+
+
+{- Steps a search result, to produce the next result.
+   * The result of this function may be an Ongoing search. This will provide the
+     current head search node and a continuation to run the remainder of the search.
+-}
+
+
+next : SearchResult state -> SearchResult state
+next result =
+    case result of
+        Complete ->
+            Complete
+
+        Goal state ->
+            Goal state
+
+        Ongoing _ cont ->
+            cont ()
+
+
+
+{- Continues a search result, to produce the next search goal.
+   * The result of this function will never be an Ongoing search. This
+     function will recursively apply the search until either a Goal state if
+     found or the walk over the search space is Complete.
+-}
+
+
+nextGoal : SearchResult state -> SearchResult state
+nextGoal result =
+    case result of
+        Complete ->
+            Complete
+
+        Goal state ->
+            Goal state
+
+        Ongoing _ cont ->
+            cont () |> nextGoal

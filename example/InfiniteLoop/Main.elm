a23ea6c1e1abf0b4e8e964646406d932376dfa37
Rupert Smith <rupert@thesett.com>
2018-02-26 16:48:48 +0000

    Made an infinite looping search space.

--- example/InfiniteLoop/Main.elm
+++ example/InfiniteLoop/Main.elm
@@ -1,5 +1,26 @@
 module Main exposing (..)
 
+import Search
+import Html exposing (text)
 
-dummy =
-    "value"
+
+main =
+    text "infinite loop timer"
+
+
+type alias State =
+    Int
+
+
+step : Search.Step State
+step state =
+    [ ( state + 1, False ) ]
+
+
+{-| Packages the infinite loop as an uninformed search.
+-}
+uninformed : Search.Uninformed State
+uninformed =
+    { step = step
+    , cost = \_ -> 1.0
+    }
